238e002c0f981da1d4a97d9c1b2d04129275d9e4
mcimildoro <cimildoro@gmail.com>
2025-02-21 19:41:01 +0100

    fix: whatsapp icon z-index

--- src/components/Whatsapp/whatsapp-button.tsx
+++ src/components/Whatsapp/whatsapp-button.tsx
@@ -6,7 +6,7 @@ const WhatsAppButton = () => {
     <Link
       href="https://wa.me/1234567890" // Reemplaza con tu número de WhatsApp
       target="_blank"
-      className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white p-3 rounded-full shadow-lg transition duration-300 ease-in-out flex items-center justify-center text-3xl"
+      className="z-50 fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white p-3 rounded-full shadow-lg transition duration-300 ease-in-out flex items-center justify-center text-3xl"
     >
       <FaWhatsapp />
     </Link>
