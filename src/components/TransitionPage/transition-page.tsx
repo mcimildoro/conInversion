f45ac531fbd5b6e4200253ce4ad332924a825ec2
mcimildoro <cimildoro@gmail.com>
2025-03-13 16:55:45 +0100

    fixt: transition color

--- src/components/TransitionPage/transition-page.tsx
+++ src/components/TransitionPage/transition-page.tsx
@@ -8,7 +8,7 @@ const TransitionPage = () => {
         <AnimatePresence mode="wait">
             <div>
                 <motion.div
-                    className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-gradient-cover"
+                    className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-green-600"
                     variants={transitionVariantsPage}
                     initial="initial"
                     animate="animate"
